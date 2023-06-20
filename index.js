e033c29fbba4aeef20a8d81c4867e563974efe39
Soyoung Lemonds <soyoung.lemonds@yahoo.com>
2023-06-20 14:43:07 -0400

    Adds index.js codes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+console.log (saturdayFun());
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+console.log (mondayWork());
+
+function wrapAdjective(symbol = '*') {
+    return function (str = "special"){
+        return (`You are ${symbol}${str}${symbol}!`);
+    } 
+}
